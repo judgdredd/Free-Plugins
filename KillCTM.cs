69cafe34ca4f4b3b5cae8f408664922d6994d571
Judgdredd <judgdr3dd@gmail.com>
2015-11-08 12:56:04 +1100

    update

--- KillCTM.cs
+++ KillCTM.cs
@@ -2,6 +2,8 @@
 using Styx.Plugins;
 using Styx.CommonBot;
 using Styx.WoWInternals;
+using Styx.Common;
+using System.Windows.Media;
 
 namespace HBtimer
 {
@@ -12,6 +14,22 @@ namespace HBtimer
         public override string Author { get { return "JudgDr3dd"; } }
         public override string Name { get { return "KillCTM"; } }
         public override Version Version { get { return new Version("0.1"); } }
+        public override void Pulse() { }
+
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            BotEvents.OnBotStopped += OnBotStoppedEvent;
+            BotEvents.OnBotPaused += OnBotPausedEvent;
+        }
+        
+        public override void OnDisable()
+        {
+            base.OnDisable();
+            BotEvents.OnBotStopped -= OnBotStoppedEvent;
+            BotEvents.OnBotPaused -= OnBotPausedEvent;
+        }
 
         private void OnBotStoppedEvent(EventArgs args)
         {
@@ -25,27 +43,8 @@ namespace HBtimer
 
         private void DisableCTM()
         {
-            if (TreeRoot.IsPaused || !TreeRoot.IsRunning)
-            Lua.DoString(string.Format("RunMacroText(\"{0}\")", "/click InterfaceOptionsMousePanelClickToMove 0"), 0);
-        }
-
-        public override void OnEnable()
-        {
-            base.OnEnable();
-            BotEvents.OnBotStopped += OnBotStoppedEvent;
-            //BotEvents.OnBotPaused += OnBotPausedEvent;
-        }
-
-        public override void OnDisable()
-        {
-            base.OnDisable();
-            BotEvents.OnBotStopped -= OnBotStoppedEvent;
-            //BotEvents.OnBotPaused -= OnBotPausedEvent;
-        }
-
-        public override void Pulse()
-        {
-            
+            Lua.DoString("SetCVar('autoInteract', '0')");
+            Logging.Write(LogLevel.Normal, Colors.Aqua, "Click to Move Disabled");
         }
 
     }
